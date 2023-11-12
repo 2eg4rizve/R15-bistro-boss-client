3293d67e7b6bf217867274894ab496cdfd6a3630
2eg4rizve <2eg4rizve.com>
2023-11-12 22:36:40 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+import Banner from "./Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+        
+            <Banner></Banner>
+            {/* <Category></Category> */}
+            
+        </div>
+    );
+};
+
+export default Home;
