91d3be5f3dccd7c058a42e265d51acfbdaed9160
2eg4rizve <2eg4rizve.com>
2023-12-30 18:42:59 +0600

    useMenu

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -3,10 +3,10 @@ import { Helmet } from 'react-helmet-async';
 import Cover from '../Shared/Cover/Cover';
 
 import menuImg from '../../assets/menu/menu-bg.jpg'
-import soupImg from '../../assets/menu/soup-bg.jpg'
-import saladImg from '../../assets/menu/salad-bg.jpg'
-import pizzaImg from '../../assets/menu/pizza-bg.jpg'
-import dessertImg from '../../assets/menu/dessert-bg.jpeg'
+// import soupImg from '../../assets/menu/soup-bg.jpg'
+// import saladImg from '../../assets/menu/salad-bg.jpg'
+// import pizzaImg from '../../assets/menu/pizza-bg.jpg'
+// import dessertImg from '../../assets/menu/dessert-bg.jpeg'
 import PopularMenu from '../Home/PopularMenu/PopularMenu';
 
 
